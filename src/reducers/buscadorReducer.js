c71484e058a2b56c9d28b0f2cd3635828725f166
carloslavie <carloslavie@gmail.com>
2021-06-25 12:19:35 -0300

    cleaning code

--- src/reducers/buscadorReducer.js
+++ src/reducers/buscadorReducer.js
@@ -28,33 +28,10 @@ export const busquedaReducer = createSlice ({
                 state.resultadoUsuarios = action.payload;
                 // state.loading = false
              },
-            // agregarTestimonios: (state, action) =>{
-            //     state.testimonios.push(action.payload);
-            //     state.loading = false                                
-            // },
-            // testimoniosFetching: (state) =>{
-            //     state.loading = true
-            // },
-            // testimonioError: (state, action) =>{
-            //     state.error = action.payload;
-            //     state.loading = false
-            // },
-            // actualizarTestimonio: (state, action) =>{
-            //     state.testimonios = state.testimonios.map(testimonio => testimonio.id === action.payload.id ? action.payload : testimonio);
-            //     state.loading = false
-                
-            // },
-            // testimonioActual: (state, action) =>{
-            //     state.testimonio = action.payload
-            // },
-            // eliminarTestimonio: (state, action)=>{
-                
-            //     state.testimonios = state.testimonios.filter(testimonio=>testimonio.id !== action.payload)                
-            // }
+            
         }
 })
 
-// export const { obtenerTestimonios, agregarTestimonios, actualizarTestimonio, eliminarTestimonio, testimonioActual, testimoniosFetching, testimonioError } = busquedaReducer.actions;
 export const { obtenerResultadosUsuarios, obtenerResultadosConciliaciones, obtenerResultadosFuentes, obtenerResultadosTableros} = busquedaReducer.actions;
 
 export const obtenerResultadosApi =  () => async dispatch => {
@@ -91,64 +68,6 @@ export const obtenerResultadosApi =  () => async dispatch => {
 
 };
 
-// export const agregarTestimonioApi = testimonio => async dispatch => {
-//     dispatch(testimoniosFetching())
-//     try {               
-//         await fetch('http://ongapi.alkemy.org/api/testimonials', {
-//             method: 'POST', 
-//             body: JSON.stringify(testimonio), 
-//             headers:{
-//               'Content-Type': 'application/json'
-//             }})
-        
-                
-//         dispatch(agregarTestimonios(testimonio))
 
-//      } catch (error) {
-//         dispatch(testimonioError(error.message));
-//     }
-//  }
-
-
-//  export const actualizarTestimonioApi = testimonio => async dispatch => {
-    
-//     dispatch(testimoniosFetching())
-//    try {
-       
-//        const resultado = await fetch(`http://ongapi.alkemy.org/api/testimonials/${testimonio.id}`, {
-//            method: 'PUT',
-//            body: JSON.stringify(testimonio), 
-//            headers:{
-//              'Content-Type': 'application/json'
-//            }})
-           
-//            const result = await resultado.json()
-           
-//         dispatch(actualizarTestimonio(result.data))
-
-//    } catch (error) {
-//     dispatch(testimonioError(error.message));
-//     console.log(error)
-// }
-// }
-
-// export const obtenerTestimonioActual = testimonio => dispatch => {
-    
-//     dispatch(testimonioActual(testimonio))
-// }
-
-// export const eliminarTestimonioApi = id => async dispatch =>{
-    
-//     try {
-//         await fetch(`http://ongapi.alkemy.org/api/testimonials/${id}`, {
-//             method: 'DELETE',
-//             })
-                                           
-//         dispatch(eliminarTestimonio(id))
-
-//     } catch (error) {
-//     dispatch(testimonioError(error.message));
-// }
-// }   
 export default busquedaReducer.reducer;
 
